919e9a6deb05e512f668f0ec3e30ef6549214ef8
Scott Lamb <slamb@slamb.org>
2017-10-04 22:55:29 -0700

    remove extraneous debug logging

--- src/mp4.rs
+++ src/mp4.rs
@@ -1418,12 +1418,10 @@ impl FileInner {
     fn get_co64(&self, r: Range<u64>, l: u64) -> Result<Chunk, Error> {
         let mut v = Vec::with_capacity(l as usize);
         let mut pos = self.initial_sample_byte_pos;
-        debug!("co64, initial pos={}", pos);
         for s in &self.segments {
             v.write_u64::<BigEndian>(pos)?;
             let r = s.s.sample_file_range();
             pos += r.end - r.start;
-            debug!("co64, pos={} after adding r={:?}", pos, r);
         }
         Ok(ARefs::new(v).map(|v| &v[r.start as usize .. r.end as usize]))
     }
