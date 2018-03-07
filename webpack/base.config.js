5727adf3dfa77223dc42ea79a0d0445b2c78bebb
Dolf Starreveld <dolf@starreveld.com>
2018-03-07 13:09:43 -0800

    Settings can now be taken from separate file with local override.
    * Various settings in settings-nvr.js module
    * settings-nvr-local.js can override settings-nvr.js
    * settings-nvr-local is unchecked file
    * Both files can be straight maps, or functions returning maps
    * webpack env and args available to those functions

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,42 +1,52 @@
+// vim: set et ts=2 sw=2:
+//
+
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NVRSettings = require('./NVRSettings');
 
+module.exports = (env, args) => {
+  const nvrSettings = new NVRSettings(env, args).settings;
 
-const project_root = path.join(__dirname, '../');
-const src_dir = path.join(project_root, 'ui-src');
-const dist_dir = path.join(project_root, 'ui-dist');
-
-module.exports = {
-  entry: {
-    nvr: path.join(src_dir, 'index.js'),
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(dist_dir),
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel',
-      query: {
-        'presets': ['env', {}],
-      },
-      include: [path.resolve(__dirname, './ui-src'), path.resolve(__dirname, './ui-src/lib')],
-    }, {
-      test: /\.png$/,
-      use: ['file-loader'],
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader',
-    }],
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/\.\/locale$/),
-    new webpack.NormalModuleReplacementPlugin(
-      /node_modules\/moment\/moment\.js$/,
-      './min/moment.min.js'),
-    new webpack.NormalModuleReplacementPlugin(
-      /node_modules\/moment-timezone\/index\.js$/,
-      './builds/moment-timezone-with-data-2012-2022.min.js'),
-  ],
+  return {
+    entry: {
+      nvr: path.join(nvrSettings._paths.app_src_dir, 'index.js'),
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: nvrSettings._paths.dist_dir,
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          'presets': ['env'],
+        },
+        exclude: /(node_modules|bower_components)/,
+        include: [ './ui-src'],
+      }, {
+        test: /\.png$/,
+        use: ['file-loader'],
+      }, {
+        // Load css and then in-line in head
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      }],
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/\.\/locale$/),
+      new HtmlWebpackPlugin({
+        title: nvrSettings.app_title,
+        template: path.join(nvrSettings._paths.app_src_dir, 'index.html'),
+      }),
+      new webpack.NormalModuleReplacementPlugin(
+        /node_modules\/moment\/moment\.js$/,
+        './min/moment.min.js'),
+      new webpack.NormalModuleReplacementPlugin(
+        /node_modules\/moment-timezone\/index\.js$/,
+        './builds/moment-timezone-with-data-2012-2022.min.js'),
+    ],
+  };
 };
