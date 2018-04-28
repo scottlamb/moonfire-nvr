scottlamb/moonfire-nvr

commit f5aa0080bb18c78246970cae49e5e89f2ba22a9d

A little more UI refactor, cleanup, eslint more strict (#54)
* A little more UI refactor, cleanup, eslint more strict

* Split out imports for jQuery components and put them where needed.
* No longer do all of it in application module.
* Prepares better for code splitting.
* Split out video player dialog
* Simplifies jquery-ui dependencies for code splitting
* Simplifies code
* Configure to generate more, but smaller bundles.
* Setup some more strict eslint settings
* Fix css to import rather than require
* Change settings to correctly support tree shaking in production build

Signed-off-by: Dolf Starreveld <dolf@starreveld.com>

* Remove “old” code from TimeFormatter

* Accidentally left behind due to overlapping PRs

Signed-off-by: Dolf Starreveld <dolf@starreveld.com>

ui-src/lib/views/VideoDialogView.js
@@ -1,0 +1,99 @@
+// vim: set et sw=2 ts=2:
+//
+// This file is part of Moonfire NVR, a security camera digital video recorder.
+// Copyright (C) 2018 Dolf Starreveld <dolf@starreveld.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// In addition, as a special exception, the copyright holders give
+// permission to link the code of portions of this program with the
+// OpenSSL library under certain conditions as described in each
+// individual source file, and distribute linked combinations including
+// the two.
+//
+// You must obey the GNU General Public License in all respects for all
+// of the code used other than OpenSSL. If you modify file(s) with this
+// exception, you may extend this exception to your version of the
+// file(s), but you are not obligated to do so. If you do not wish to do
+// so, delete this exception statement from your version. If you delete
+// this exception statement from all source files in the program, then
+// also delete it here.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import $ from 'jquery';
+
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/dialog.css';
+import 'jquery-ui/themes/base/theme.css';
+// This not needed for pure dialog, but we want it resizable
+import 'jquery-ui/themes/base/resizable.css';
+
+// Get dialog ui widget
+import 'jquery-ui/ui/widgets/dialog';
+
+/**
+ * Class to implement a simple jQuery dialog based video player.
+ */
+export default class VideoDialogView {
+  /**
+   * Construct the player.
+   *
+   * This does not attach the player to the DOM anywhere! In fact, construction
+   * of the necessary video element is delayed until an attach is requested.
+   * Since the close of the video removes all traces of it in the DOM, this
+   * apprach allows repeated use by calling attach again!
+   */
+  constructor() {}
+
+  /**
+   * Attach the player to the specified DOM element.
+   *
+   * @param {Node} domElement DOM element to attach to
+   * @return {VideoDialogView} Returns "this" for chaining.
+   */
+  attach(domElement) {
+    this._videoElement = $('<video controls preload="auto" autoplay="true" />');
+    this._dialogElement = $('<div class="playdialog" />').append(
+      this._videoElement
+    );
+    $(domElement).append(this._dialogElement);
+    return this;
+  }
+
+  /**
+   * Show the player, and start playing the given url.
+   *
+   * @param  {String} title Title of the video player
+   * @param  {Number} width Width of the player
+   * @param  {String} url   URL for source media
+   * @return {VideoDialogView}       Returns "this" for chaining.
+   */
+  play(title, width, url) {
+    this._dialogElement.dialog({
+      title: title,
+      width: width,
+      close: () => {
+        const videoDOMElement = this._videoElement[0];
+        videoDOMElement.pause();
+        videoDOMElement.src = ''; // Remove current source to stop loading
+        this._videoElement = null;
+        this._dialogElement.remove();
+        this._dialogElement = null;
+      },
+    });
+    // Now that dialog is up, set the src so video starts
+    console.log('Video url: ' + url);
+    this._videoElement.attr('src', url);
+    return this;
+  }
+}

webpack/base.config.js
@@ -48,32 +48,41 @@ module.exports = (env, args) => {
       publicPath: '/',
     },
     module: {
-      rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          'presets': ['env'],
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', {modules: false}],
+          },
+          exclude: /(node_modules|bower_components)/,
+          include: ['./ui-src'],
         },
-        exclude: /(node_modules|bower_components)/,
-        include: ['./ui-src'],
-      }, {
-        test: /\.png$/,
-        use: ['file-loader'],
-      }, {
-        test: /\.ico$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      }, {
-        // Load css and then in-line in head
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      }],
+        {
+          test: /\.png$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.ico$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.png$/,
+          use: ['file-loader'],
+        },
+        {
+          // Load css and then in-line in head
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
     plugins: [
       new webpack.DefinePlugin({
@@ -83,14 +92,20 @@ module.exports = (env, args) => {
       new HtmlWebpackPlugin({
         title: nvrSettings.app_title,
         filename: 'index.html',
-        template: path.join(nvrSettings._paths.app_src_dir, 'assets', 'index.html'),
+        template: path.join(
+          nvrSettings._paths.app_src_dir,
+          'assets',
+          'index.html'
+        ),
       }),
       new webpack.NormalModuleReplacementPlugin(
         /node_modules\/moment\/moment\.js$/,
-        './min/moment.min.js'),
+        './min/moment.min.js'
+      ),
       new webpack.NormalModuleReplacementPlugin(
         /node_modules\/moment-timezone\/index\.js$/,
-        './builds/moment-timezone-with-data-2012-2022.min.js'),
+        './builds/moment-timezone-with-data-2012-2022.min.js'
+      ),
     ],
   };
 };

ui-src/NVRApplication.js
@@ -34,31 +34,32 @@
 // TODO: add error bar on fetch failure.
 // TODO: live updating.
 
-import 'jquery-ui/themes/base/button.css';
+import $ from 'jquery';
+
+// tooltip needs:
+// css.structure: ../../themes/base/core.css
+// css.structure: ../../themes/base/tooltip.css
+// css.theme: ../../themes/base/theme.css
+
 import 'jquery-ui/themes/base/core.css';
-import 'jquery-ui/themes/base/datepicker.css';
-import 'jquery-ui/themes/base/dialog.css';
-import 'jquery-ui/themes/base/resizable.css';
-import 'jquery-ui/themes/base/theme.css';
 import 'jquery-ui/themes/base/tooltip.css';
+import 'jquery-ui/themes/base/theme.css';
 
 // This causes our custom css to be loaded after the above!
-require('./assets/index.css');
+import './assets/index.css';
 
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/datepicker';
-import 'jquery-ui/ui/widgets/dialog';
+// Get ui widgets themselves
 import 'jquery-ui/ui/widgets/tooltip';
 
 import Camera from './lib/models/Camera';
 import CameraView from './lib/views/CameraView';
 import CalendarView from './lib/views/CalendarView';
+import VideoDialogView from './lib/views/VideoDialogView';
 import NVRSettingsView from './lib/views/NVRSettingsView';
 import CheckboxGroupView from './lib/views/CheckboxGroupView';
 import RecordingFormatter from './lib/support/RecordingFormatter';
-import TimeFormatter, {
-  TimeStamp90kFormatter,
-} from './lib/support/TimeFormatter';
+import TimeFormatter from './lib/support/TimeFormatter';
+import TimeStamp90kFormatter from './lib/support/TimeStamp90kFormatter';
 import MoonfireAPI from './lib/MoonfireAPI';
 
 const api = new MoonfireAPI();
@@ -130,27 +131,18 @@ function onSelectVideo(nvrSettingsView, camera, range, recording) {
     trimmedRange,
     nvrSettingsView.timeStampTrack
   );
-  const video = $('<video controls preload="auto" autoplay="true" />');
-  const dialog = $('<div class="playdialog" />').append(video);
-  $('body').append(dialog);
-
-  let [formattedStart, formattedEnd] = timeFormatter90k.formatSameDayShortened(
+  const [
+    formattedStart,
+    formattedEnd,
+  ] = timeFormatter90k.formatSameDayShortened(
     trimmedRange.startTime90k,
     trimmedRange.endTime90k
   );
-  dialog.dialog({
-    title: camera.shortName + ', ' + formattedStart + ' to ' + formattedEnd,
-    width: recording.videoSampleEntryWidth / 4,
-    close: () => {
-        const videoDOMElement = video[0];
-        videoDOMElement.pause();
-        videoDOMElement.src = ''; // Remove current source to stop loading
-        dialog.remove();
-    },
-  });
-  // Now that dialog is up, set the src so video starts
-  console.log('Video url: ' + url);
-  video.attr('src', url);
+  const videoTitle =
+    camera.shortName + ', ' + formattedStart + ' to ' + formattedEnd;
+  new VideoDialogView()
+    .attach($('body'))
+    .play(videoTitle, recording.videoSampleEntryWidth / 4, url);
 }
 
 /**
@@ -193,16 +185,19 @@ function fetch(selectedRange, videoLength) {
       cameraView.recordingsReq = null;
     });
     r
-      .then(function(data, status, req) {
+      .then(function(data /* , status, req */) {
         // Sort recordings in descending order.
         data.recordings.sort(function(a, b) {
           return b.startId - a.startId;
         });
-        console.log('Fetched results > updating recordings');
+        console.log(
+          'Fetched results for "%s" > updating recordings',
+          cameraView.camera.shortName
+        );
         cameraView.recordingsJSON = data.recordings;
       })
       .catch(function(data, status, err) {
-        console.log(url, ' load failed: ', status, ': ', err);
+        console.error(url, ' load failed: ', status, ': ', err);
       });
   }
 }
@@ -295,11 +290,11 @@ export default class NVRApplication {
       .request(api.nvrUrl(true))
       .done((data) => onReceivedCameras(data))
       .fail((req, status, err) => {
-        console.log('NVR load error: ', status, err);
+        console.error('NVR load error: ', status, err);
         onReceivedCameras({cameras: []});
       })
       .catch((e) => {
-        console.log('NVR load exception: ', e);
+        console.error('NVR load exception: ', e);
         onReceivedCameras({cameras: []});
       });
   }

ui-src/assets/index.css
@@ -4,7 +4,8 @@ body {
 #nav {
     float: left;
 }
-.ui-datepicker {
+
+#datetime .ui-datepicker {
     width: 100%;
 }
 

ui-src/index.js
@@ -33,6 +33,7 @@
 import NVRApplication from './NVRApplication';
 
 import $ from 'jquery';
+import './favicon.ico';
 
 // On document load, start application
 $(function() {

ui-src/lib/MoonfireAPI.js
@@ -59,7 +59,7 @@ export default class MoonfireAPI {
     url.hostname = host;
     url.port = port;
     console.log('API: ' + url.origin + url.pathname);
-    this._builder = new URLBuilder(url.origin + url.pathname);
+    this._builder = new URLBuilder(url.origin + url.pathname, relativeUrls);
   }
 
   /**

ui-src/lib/models/CalendarTSRange.js
@@ -31,7 +31,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import Time90kParser from '../support/Time90kParser';
-import {TimeStamp90kFormatter} from '../support/TimeFormatter';
+import TimeStamp90kFormatter from '../support/TimeStamp90kFormatter';
 import Range90k from './Range90k';
 
 /**

ui-src/lib/models/Range.js
@@ -45,7 +45,7 @@ export default class Range {
    */
   constructor(low, high) {
     if (high < low) {
-      console.log('Warning range swap: ' + low + ' - ' + high);
+      console.warn('Warning range swap: ' + low + ' - ' + high);
       [low, high] = [high, low];
     }
     this.low = low;
@@ -65,7 +65,7 @@ export default class Range {
    * Determine if value is inside the range.
    *
    * @param  {Number}  value Value to test
-   * @return {Boolean}
+   * @return {Boolean} 
    */
   isInRange(value) {
     return value >= this.low && value <= this.high;

commit f2888e2b3242c00e18a0a2dad8b9529beb316a83

couple fixes to the dialog close button
* add back in button.css (broken with f5aa008)
* remove a redundant .png file-loader which apparently caused the .png
  asset to not load properly (broken with f5aa008)

ui-src/lib/views/VideoDialogView.js
@@ -32,6 +32,7 @@
 
 import $ from 'jquery';
 
+import 'jquery-ui/themes/base/button.css';
 import 'jquery-ui/themes/base/core.css';
 import 'jquery-ui/themes/base/dialog.css';
 import 'jquery-ui/themes/base/theme.css';

webpack/base.config.js
@@ -74,10 +74,6 @@ module.exports = (env, args) => {
           ],
         },
         {
-          test: /\.png$/,
-          use: ['file-loader'],
-        },
-        {
           // Load css and then in-line in head
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],