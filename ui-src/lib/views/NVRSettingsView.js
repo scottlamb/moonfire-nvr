scottlamb/moonfire-nvr

commit 58152e8d94e0088b599c05a09b3476db95738562

Major refactoring of UI code, small UI changes. (#48)
* Major refactoring of UI code, small UI changes.

* Single file index.js split up into separate modules
* Modules for handling UI view components
* Modules for handling JSON/Model data
* Modules for support tasks
* Module to encapsulate Moonfire API
* Main application module
* index.js simplified to just activating main app
* Settings file functionality expanded
* UI adds "Time Format" popup to allow changing time representation
* CSS changes/additions to streamline looks
* Recordings loading indicator only appears after 500ms delay, if at all

* Address first set of PR change requests from Scott.

* Add copyright headers to all files (except JSON files)
* Fix bug with entering time values in range pickers
* Fixed an erroneous comment and/or spelling error here and there
* Fixed JSDoc comments where [description] was not filled in
* Removed a TODO from NVRApplication as it no longer applies
* Fixed bug handling "infinite" case of video segment lengths
* Fixed bug in "trim" handler and trim execution

* Retrofit video continues loading from separate PR

Signed-off-by: Dolf Starreveld <dolf@starreveld.com>

* Address PR comments

Signed-off-by: Dolf Starreveld <dolf@starreveld.com>

* Address PR comments

Signed-off-by: Dolf Starreveld <dolf@starreveld.com>

ui-src/lib/views/NVRSettingsView.js
@@ -1,0 +1,205 @@
+// vim: set et sw=2 ts=2:
+//
+// This file is part of Moonfire NVR, a security camera digital video recorder.
+// Copyright (C) 2018 Dolf Starreveld <dolf@starreveld.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// In addition, as a special exception, the copyright holders give
+// permission to link the code of portions of this program with the
+// OpenSSL library under certain conditions as described in each
+// individual source file, and distribute linked combinations including
+// the two.
+//
+// You must obey the GNU General Public License in all respects for all
+// of the code used other than OpenSSL. If you modify file(s) with this
+// exception, you may extend this exception to your version of the
+// file(s), but you are not obligated to do so. If you do not wish to do
+// so, delete this exception statement from your version. If you delete
+// this exception statement from all source files in the program, then
+// also delete it here.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import $ from 'jquery';
+
+/**
+ * Class to control the view of NVR Settings.
+ *
+ * These  settings/controls include:
+ * - Max video length
+ * - Trim segment start/end
+ * - Time Format
+ */
+export default class NVRSettingsView {
+  /**
+   * Construct based on element ids
+   */
+  constructor({
+    videoLenId = 'split',
+    trimCheckId = 'trim',
+    tsTrackId = 'ts',
+    timeFmtId = 'timefmt',
+  } = {}) {
+    this._ids = {videoLenId, trimCheckId, tsTrackId, timeFmtId};
+    this._videoLength = null;
+    this._videoLengthHandler = null;
+    this._trim = null;
+    this._trimHandler = null;
+    this._timeFmtStr = null;
+    this._timeFmtHandler = null;
+    this._tsTrack = null;
+    this._tsTrackHandler = null;
+    this._wireControls();
+  }
+
+  /**
+   * Find selected option in <select> and return value, or first option's value.
+   *
+   * The first option's value is returned if no option is selected.
+   *
+   * @param  {jQuery} selectEl jQuery element for the <select>
+   * @return {String}          Value of the selected/first option
+   */
+  _findSelectedOrFirst(selectEl) {
+    let value = selectEl.find(':selected').val();
+    if (!value) {
+      value = selectEl.find('option:first-child').val();
+    }
+    return value;
+  }
+
+  /**
+   * Wire up all controls and handlers.
+   *
+   */
+  _wireControls() {
+    const videoLengthEl = $(`#${this._ids.videoLenId}`);
+    this._videoLength = this._findSelectedOrFirst(videoLengthEl);
+    videoLengthEl.change((e) => {
+      const newValueStr = e.currentTarget.value;
+      this._videoLength =
+        newValueStr == 'infinite' ? Infinity : Number(newValueStr);
+      if (this._videoLengthHandler) {
+        this._videoLengthHandler(this._videoLength);
+      }
+    });
+
+    const trimEl = $(`#${this._ids.trimCheckId}`);
+    this._trim = trimEl.is(':checked');
+    trimEl.change((e) => {
+      this._trim = e.currentTarget.checked;
+      if (this._trimHandler) {
+        this._trimHandler(this._trim);
+      }
+    });
+
+    const timeFmtEl = $(`#${this._ids.timeFmtId}`);
+    this._timeFmtStr = this._findSelectedOrFirst(timeFmtEl);
+    timeFmtEl.change((e) => {
+      this._timeFmtStr = e.target.value;
+      if (this._timeFmtHandler) {
+        this._timeFmtHandler(this._timeFmtStr);
+      }
+    });
+
+    const trackEl = $(`#${this._ids.tsTrackId}`);
+    this._tsTrack = trackEl.is(':checked');
+    trackEl.change((e) => {
+      this._tsTrack = e.target.checked;
+      if (this._tsTrackHandler) {
+        this._tsTrackHandler(this._tsTrack);
+      }
+    });
+  }
+
+  /**
+   * Get currently selected video length.
+   *
+   * @return {Number} Video length value
+   */
+  get videoLength() {
+    return this._videoLength;
+  }
+
+  /**
+   * Get currently selected time format string.
+   *
+   * @return {String} Format string
+   */
+  get timeFormatString() {
+    return this._timeFmtStr;
+  }
+
+  /**
+   * Get currently selected trim setting.
+   *
+   * @return {Boolean}
+   */
+  get trim() {
+    return this._trim;
+  }
+
+  /**
+   * Determine value of timestamp tracking option
+   *
+   * @return {Boolean}
+   */
+  get timeStampTrack() {
+    return this._tsTrack;
+  }
+
+  /**
+   * Set a handler to be called when the time format string changes.
+   *
+   * The handler will be called with one argument: the new format string.
+   *
+   * @param  {Function} handler Format change handler
+   */
+  set onTimeFormatChange(handler) {
+    this._timeFmtHandler = handler;
+  }
+
+  /**
+   * Set a handler to be called when video length popup changes.
+   *
+   * The handler will be called with one argument: the new video length.
+   *
+   * @param  {Function} handler Video Length change handler
+   */
+  set onVideoLengthChange(handler) {
+    this._videoLengthHandler = handler;
+  }
+
+  /**
+   * Set a handler to be called when video trim checkbox changes.
+   *
+   * The handler will be called with one argument: the new trim value (Boolean).
+   *
+   * @param  {Function} handler Trim change handler
+   */
+  set onTrimChange(handler) {
+    this._trimHandler = handler;
+  }
+
+  /**
+   * Set a handler to be called when video timestamp tracking checkbox changes.
+   *
+   * The handler will be called with one argument: the new tsTrack value
+   * (Boolean).
+   *
+   * @param  {Function} handler Timestamp track change handler
+   */
+  set onTimeStampTrackChange(handler) {
+    this._tsTrackHandler = handler;
+  }
+}

.gitignore
@@ -3,6 +3,7 @@ cameras.sql
 .project
 .settings
 *.swp
+*.sublime-workspace
 node_modules
 prep.config
 settings-nvr-local.js

.jsbeautifyrc
@@ -1,0 +1,43 @@
+{
+    "indent_size": 2,
+    "indent_char": " ",
+    "indent_with_tabs": false,
+    "eol": "\n",
+    "end_with_newline": true,
+    "indent_level": 0,
+    "preserve_newlines": true,
+    "max_preserve_newlines": 4,
+    "space_in_paren": false,
+    "space_in_empty_paren": false,
+    "jslint_happy": false,
+    "space_after_anon_function": false,
+    "brace_style": "collapse,preserve-inline",
+    "unindent_chained_methods": false,
+    "break_chained_methods": false,
+    "keep_array_indentation": false,
+    "unescape_strings": false,
+    "wrap_line_length": 0,
+    "e4x": false,
+    "comma_first": false,
+    "operator_position": "before-newline",
+    "js": {
+        "indent_size": 2,
+        "indent_char": " ",
+        "indent_with_tabs": false
+    },
+    "json": {
+       "brace_style": "expand",
+        "keep_array_indentation": false,
+        "unescape_strings": false
+    },
+    "custom": {
+        "package?(-lock).json": {
+            "indent_size": 2,
+            "brace_style": "collapse"
+        },
+        "*.sublime-@(settings|keymap|commands|menu)": {
+            "indent_size": 4,
+            "brace_style": "expand"
+        }
+    }
+}

moonfire.sublime-project
@@ -1,0 +1,42 @@
+{
+  "folders": [
+    {
+      "path": ".",
+      "folder_exclude_patterns": [
+        "design",
+        "ffmpeg",
+        "node_modules",
+        "ui-dist"
+      ],
+      "file_exclude_patterns": ["*.png", "*.jpg", "*.gif"]
+    }
+  ],
+  "settings": {
+    "js_prettier": {
+      "additional_cli_args": {
+        "--config-precedence": "prefer-file"
+      },
+      "debug": true,
+      "prettier_cli_path": "",
+      "node_path": "",
+      "auto_format_on_save": false,
+      "auto_format_on_save_excludes": [],
+      "auto_format_on_save_requires_prettier_config": false,
+      "allow_inline_formatting": false,
+      "custom_file_extensions": [],
+      "max_file_size_limit": -1,
+      "prettier_options": {
+        "printWidth": 80,
+        "singleQuote": false,
+        "trailingComma": "none",
+        "bracketSpacing": true,
+        "jsxBracketSameLine": false,
+        "parser": "babylon",
+        "semi": true,
+        "requirePragma": false,
+        "proseWrap": "preserve",
+        "arrowParens": "avoid"
+      }
+    }
+  }
+}

ui-src/NVRApplication.js
@@ -1,0 +1,306 @@
+// vim: set et sw=2 ts=2:
+//
+// This file is part of Moonfire NVR, a security camera digital video recorder.
+// Copyright (C) 2018 Dolf Starreveld <dolf@starreveld.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// In addition, as a special exception, the copyright holders give
+// permission to link the code of portions of this program with the
+// OpenSSL library under certain conditions as described in each
+// individual source file, and distribute linked combinations including
+// the two.
+//
+// You must obey the GNU General Public License in all respects for all
+// of the code used other than OpenSSL. If you modify file(s) with this
+// exception, you may extend this exception to your version of the
+// file(s), but you are not obligated to do so. If you do not wish to do
+// so, delete this exception statement from your version. If you delete
+// this exception statement from all source files in the program, then
+// also delete it here.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// TODO: test abort.
+// TODO: add error bar on fetch failure.
+// TODO: live updating.
+
+import 'jquery-ui/themes/base/button.css';
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/datepicker.css';
+import 'jquery-ui/themes/base/dialog.css';
+import 'jquery-ui/themes/base/resizable.css';
+import 'jquery-ui/themes/base/theme.css';
+import 'jquery-ui/themes/base/tooltip.css';
+
+// This causes our custom css to be loaded after the above!
+require('./assets/index.css');
+
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/datepicker';
+import 'jquery-ui/ui/widgets/dialog';
+import 'jquery-ui/ui/widgets/tooltip';
+
+import Camera from './lib/models/Camera';
+import CameraView from './lib/views/CameraView';
+import CalendarView from './lib/views/CalendarView';
+import NVRSettingsView from './lib/views/NVRSettingsView';
+import CheckboxGroupView from './lib/views/CheckboxGroupView';
+import RecordingFormatter from './lib/support/RecordingFormatter';
+import TimeFormatter, {
+  TimeStamp90kFormatter,
+} from './lib/support/TimeFormatter';
+import MoonfireAPI from './lib/MoonfireAPI';
+
+const api = new MoonfireAPI();
+let cameraViews = null; // CameraView objects
+let calendarView = null; // CalendarView object
+
+/**
+ * Currently selected time format specification.
+ *
+ * @type {String}
+ */
+let timeFmt = 'YYYY-MM-DD HH:mm:ss';
+
+/**
+ * Currently active time formatter.
+ * This is lazy initialized at the point we receive the timezone information
+ * and never changes afterwards, except possibly for changing the timezone.
+ *
+ * @type {[type]}
+ */
+let timeFormatter = null;
+
+/**
+ * Currently active time formatter for internal time format.
+ * This is lazy initialized at the point we receive the timezone information
+ * and never changes afterwards, except possibly for changing the timezone.
+ *
+ * @type {[type]}
+ */
+let timeFormatter90k = null;
+
+/**
+ * Globally set a new timezone for the app.
+ *
+ * @param  {String} timeZone Timezone name
+ */
+function newTimeZone(timeZone) {
+  timeFormatter = new TimeFormatter(timeFmt, timeZone);
+  timeFormatter90k = new TimeStamp90kFormatter(timeZone);
+}
+
+/**
+ * Globally set a new time format for the app.
+ *
+ * @param  {String} format Time format specification
+ */
+function newTimeFormat(format) {
+  timeFormatter = new TimeFormatter(format, timeFormatter.tz);
+}
+
+/**
+ * Event handler for clicking on a video.
+ *
+ * A 'dialog' object is attached to the body of the dom and it
+ * properly initialized with the corrcet src url.
+ *
+ * @param  {NVRSettings} nvrSettingsView NVRSettingsView in effect
+ * @param  {object} camera Object for the camera
+ * @param  {object} range Range Object
+ * @param  {object} recording Recording object
+ * @return {void}
+ */
+function onSelectVideo(nvrSettingsView, camera, range, recording) {
+  console.log('Recording clicked: ', recording);
+  const trimmedRange = recording.range90k(nvrSettingsView.trim ? range : null);
+  const url = api.videoPlayUrl(
+    camera.uuid,
+    recording,
+    trimmedRange,
+    nvrSettingsView.timeStampTrack
+  );
+  const video = $('<video controls preload="auto" autoplay="true" />');
+  const dialog = $('<div class="playdialog" />').append(video);
+  $('body').append(dialog);
+
+  let [formattedStart, formattedEnd] = timeFormatter90k.formatSameDayShortened(
+    trimmedRange.startTime90k,
+    trimmedRange.endTime90k
+  );
+  dialog.dialog({
+    title: camera.shortName + ', ' + formattedStart + ' to ' + formattedEnd,
+    width: recording.videoSampleEntryWidth / 4,
+    close: () => {
+        const videoDOMElement = video[0];
+        videoDOMElement.pause();
+        videoDOMElement.src = ''; // Remove current source to stop loading
+        dialog.remove();
+    },
+  });
+  // Now that dialog is up, set the src so video starts
+  console.log('Video url: ' + url);
+  video.attr('src', url);
+}
+
+/**
+ * Fetch camera view data for a given date/time range.
+ *
+ * @param  {Range90k} selectedRange Desired time range
+ * @param  {Number} videoLength Desired length of video segments, or Infinity
+ */
+function fetch(selectedRange, videoLength) {
+  if (selectedRange.startTime90k === null) {
+    return;
+  }
+  console.log(
+    'Fetching> ' +
+      selectedRange.formatTimeStamp90k(selectedRange.startTime90k) +
+      ' to ' +
+      selectedRange.formatTimeStamp90k(selectedRange.endTime90k)
+  );
+  for (let cameraView of cameraViews) {
+    let url = api.recordingsUrl(
+      cameraView.camera.uuid,
+      selectedRange.startTime90k,
+      selectedRange.endTime90k,
+      videoLength
+    );
+    if (cameraView.recordingsReq !== null) {
+      /*
+       * If there is another request, it would be because settings changed
+       * and so an abort is to make room for this new request, now necessary
+       * for the changed situation.
+       */
+      cameraView.recordingsReq.abort();
+    }
+    cameraView.delayedShowLoading(500);
+    let r = api.request(url);
+    cameraView.recordingsUrl = url;
+    cameraView.recordingsReq = r;
+    cameraView.recordingsRange = selectedRange.range90k();
+    r.always(function() {
+      cameraView.recordingsReq = null;
+    });
+    r
+      .then(function(data, status, req) {
+        // Sort recordings in descending order.
+        data.recordings.sort(function(a, b) {
+          return b.startId - a.startId;
+        });
+        console.log('Fetched results > updating recordings');
+        cameraView.recordingsJSON = data.recordings;
+      })
+      .catch(function(data, status, err) {
+        console.log(url, ' load failed: ', status, ': ', err);
+      });
+  }
+}
+
+/**
+ * Initialize the page after receiving camera data.
+ *
+ * Sets the following globals:
+ * zone - timezone from data received
+ * cameraViews - array of views, one per camera
+ *
+ * Builds the dom for the left side controllers
+ *
+ * @param  {Object} data JSON resulting from the main API request /api/?days=
+ */
+function onReceivedCameras(data) {
+  newTimeZone(data.timeZoneName);
+
+  // Set up controls and values
+  const nvrSettingsView = new NVRSettingsView();
+  nvrSettingsView.onVideoLengthChange = (vl) =>
+    fetch(calendarView.selectedRange, vl);
+  nvrSettingsView.onTimeFormatChange = (format) =>
+    cameraViews.forEach((view) => (view.timeFormat = format));
+  nvrSettingsView.onTrimChange = (t) =>
+    cameraViews.forEach((view) => (view.trimmed = t));
+  newTimeFormat(nvrSettingsView.timeFormatString);
+
+  calendarView = new CalendarView({timeZone: timeFormatter.tz});
+  calendarView.onRangeChange = (selectedRange) =>
+    fetch(selectedRange, nvrSettingsView.videoLength);
+
+  const camerasParent = $('#cameras');
+  const videos = $('#videos');
+
+  cameraViews = data.cameras.map((cameraJson) => {
+    const camera = new Camera(cameraJson);
+    const cv = new CameraView(
+      camera,
+      new RecordingFormatter(timeFormatter.formatStr, timeFormatter.tz),
+      nvrSettingsView.trim,
+      videos
+    );
+    cv.onRecordingClicked = (recordingModel) => {
+      console.log('Recording clicked', recordingModel);
+      onSelectVideo(
+        nvrSettingsView,
+        camera,
+        calendarView.selectedRange,
+        recordingModel
+      );
+    };
+    return cv;
+  });
+
+  // Create camera enable checkboxes
+  const cameraCheckBoxes = new CheckboxGroupView(
+    cameraViews.map((cv) => ({
+      id: cv.camera.uuid,
+      checked: true,
+      text: cv.camera.shortName,
+      camView: cv,
+    })),
+    camerasParent
+  );
+  cameraCheckBoxes.onCheckChange = (groupEl) => {
+    groupEl.camView.enabled = groupEl.checked;
+    calendarView.initializeWith(cameraViews);
+  };
+
+  calendarView.initializeWith(cameraViews);
+
+  console.log('Loaded: ' + cameraViews.length + ' camera views');
+}
+
+/**
+ * Class representing the entire application.
+ */
+export default class NVRApplication {
+  /**
+   * Construct the application object.
+   */
+  constructor() {}
+
+  /**
+   * Start the application.
+   */
+  start() {
+    api
+      .request(api.nvrUrl(true))
+      .done((data) => onReceivedCameras(data))
+      .fail((req, status, err) => {
+        console.log('NVR load error: ', status, err);
+        onReceivedCameras({cameras: []});
+      })
+      .catch((e) => {
+        console.log('NVR load exception: ', e);
+        onReceivedCameras({cameras: []});
+      });
+  }
+}

ui-src/assets/index.css
@@ -1,0 +1,76 @@
+body {
+    font-family: Arial, Helvetica, sans-serif;
+}
+#nav {
+    float: left;
+}
+.ui-datepicker {
+    width: 100%;
+}
+
+#videos {
+    display: inline-block;
+    padding-top: 0.5em;
+    padding-left: 1em;
+    padding-right: 1em;
+}
+#videos tbody:after {
+    content: "";
+    display: block;
+    height: 3ex;
+}
+table.videos {
+    border-collapse: collapse;
+}
+tbody tr.name {
+    font-size: 110%;
+    background-color: #eee;
+}
+tbody tr.name th {
+  border-bottom: 1px solid #666;
+}
+tbody tr.hdr {
+  color: #555;
+    font-size: 90%;
+}
+tr.r:hover {
+    background-color: #ddd;
+}
+tr.r td {
+    font-size: 80%;
+    cursor: pointer;
+}
+tr.r th,
+tr.r td {
+  border: 0;
+    padding: 0.5ex 1.5em;
+    text-align: right;
+}
+
+fieldset {
+  font-size: 80%;
+}
+fieldset legend {
+  font-size: 120%;
+  font-weight: bold;
+}
+
+#from, #to {
+    padding-right: 0.75em;
+}
+#st {
+    width: 100%;
+}
+
+#range {
+    padding: 0.5em 0;
+}
+.ui-dialog .ui-dialog-content {
+    overflow: visible; /* remove stupid scroll bars when resizing. */
+    padding: 0;
+}
+
+video {
+    width: 100%;
+    height: 100%;
+}

ui-src/assets/index.html
@@ -1,0 +1,76 @@
+<!DOCTYPE html>
+<!-- vim: set et: -->
+<html lang="en">
+  <head>
+    <title>Moonfire NVR</title>
+  </head>
+  <body>
+    <div id="nav">
+      <form action="#">
+        <fieldset id="cameras">
+          <legend>Cameras</legend>
+        </fieldset>
+        <fieldset id="datetime">
+          <legend>Date &amp; Time Range</legend>
+          <div id="from">
+             <div id="start-date"></div>
+             <div id="st">
+           <label for="start-time">Time:</label>
+            <input id="start-time" name="start-time" type="text" title="Starting
+            time within the day. Blank for the beginning of the day. Otherwise
+            HH:mm[:ss[:FFFFF]][+-HH:mm], where F is 90,000ths of a second.
+            Timezone is normally left out; it's useful once a year during the
+            ambiguous times of the &quot;fall back&quot; hour."></div>
+          </div>
+          <div id="range">Range:
+            <input type="radio" name="end-date-type" id="end-date-same" checked>
+            <label for="end-date-same">Single Day</label>
+            <input type="radio" name="end-date-type" id="end-date-other">
+            <label for="end-date-other">Multi Day</label><br/>
+          </div>
+          <div id="to">
+            <div id="end-date"></div>
+           <label for="start-time">Time:</label>
+            <input id="end-time" name="end-time" type="text" title="Ending
+            time within the day. Blank for the end of the day. Otherwise
+            HH:mm[:ss[:FFFFF]][+-HH:mm], where F is 90,000ths of a second.
+            Timezone is normally left out; it's useful once a year during the
+            ambiguous times of the &quot;fall back&quot; hour.">
+          </div>
+        </fieldset>
+        <fieldset>
+        <legend>Recordings Display</legend>
+        <label for="split">Max Video Duration:</label>
+        <select name="split" id="split">
+          <option value="324000000">1 hour</option>
+          <option value="1296000000">4 hours</option>
+          <option value="7776000000">24 hours</option>
+          <option value="infinite">infinite</option>
+        </select><br>
+        <input type="checkbox" checked id="trim" name="trim">
+        <label for="trim">Trim Segment Start &amp; End</label><br>
+        <input type="checkbox" checked id="ts" name="ts">
+        <label for="ts">Timestamp Track</label><br>
+        <label for="timefmt">Time Format:</label>
+        <select name="timefmt" id="timefmt">
+          <option value="MM/DD/YY hh:mm A">US-short</option>
+          <option value="MM/DD/YYYY hh:mm:ss A">US</option>
+          <option value="MM/DD/YY HH:mm" selected>Military-short</option>
+          <option value="MM/DD/YYYY HH:mm:ss">Military</option>
+          <option value="DD.MM.YY HH:mm">EU-short</option>
+          <option value="DD-MM-YYYY HH:mm:ss">EU</option>
+          <option value="YY-MM-DD hh:mm A">ISO-short (12h)</option>
+          <option value="YY-MM-DD HH:mm">ISO-short (24h)</option>
+          <option value="YYYY-MM-DD hh:mm:ss A">ISO (12h)</option>
+          <option value="YYYY-MM-DD HH:mm:ss">ISO (24h)</option>
+          <option value="YYYY-MM-DD HH:mm:ss">ISO 8601-like (No TZ)</option>
+          <option value="YYYY-MM-DDTHH:mm:ss">ISO 8601 (No TZ)</option>
+          <option value="YYYY-MM-DDTHH:mm:ssZ">ISO 8601</option>
+          <option value="YYYY-MM-DDTHH:mm:ss:FFFFFZ">Internal</option>
+        </select>
+      </fieldset>
+      </form>
+    </div>
+  <table id="videos"></table>
+</body>
+</html>

ui-src/index.html
@@ -1,79 +1,0 @@
-<!DOCTYPE html>
-<!-- vim: set et: -->
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
-    <title><%= htmlWebpackPlugin.options.title %></title>
-    <style type="text/css">
-      #nav {
-        float: left;
-        width: 17em;
-      }
-      #nav .ui-datepicker { width: 100%; }
-
-      #videos {
-        margin-left: 18em;
-      }
-      #videos tbody:after {
-        content: '';
-        display: block;
-        height: 3ex;
-      }
-      tbody .name {
-        background-color: #eee;
-      }
-      tr.r:hover { background-color: #ddd; }
-      tr.r th, tr.r td { padding: 0.5ex 1.5em; text-align: right; }
-
-      .ui-dialog .ui-dialog-content {
-        overflow: visible; /* remove stupid scroll bars when resizing. */
-        padding: 0;
-      }
-      video {
-        width: 100%;
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="nav">
-      <form action="#">
-        <fieldset id="cameras">
-          <legend>Cameras</legend>
-        </fieldset>
-        <fieldset id="datetime">
-          <legend>Datetime range</legend>
-          <div id="start-date"></div>
-          <input id="start-time" name="start-time" type="text" title="Starting
-          time within the day. Blank for the beginning of the day. Otherwise
-          HH:mm[:ss[:FFFFF]][+-HH:mm], where F is 90,000ths of a second.
-          Timezone is normally left out; it's useful once a year during the
-          ambiguous times of the &quot;fall back&quot; hour."><br>
-          <input type="radio" name="end-date-type" id="end-date-same" checked>
-          <label for="end-date-same">to same day</label><br>
-          <input type="radio" name="end-date-type" id="end-date-other">
-          <label for="end-date-other">to other day</label>
-          <div id="end-date"></div>
-          <input id="end-time" name="end-time" type="text" title="Ending
-          time within the day. Blank for the end of the day. Otherwise
-          HH:mm[:ss[:FFFFF]][+-HH:mm], where F is 90,000ths of a second.
-          Timezone is normally left out; it's useful once a year during the
-          ambiguous times of the &quot;fall back&quot; hour."><br>
-        </fieldset>
-        <label for="split">Max video length</label>
-        <select name="split" id="split">
-          <option value="324000000">1 hour</option>
-          <option value="1296000000">4 hours</option>
-          <option value="7776000000">24 hours</option>
-          <option value="">infinite</option>
-        </select><br>
-        <input type="checkbox" checked id="trim" name="trim">
-        <label for="trim">Trim segment start/end</label><br>
-        <input type="checkbox" checked id="ts" name="ts">
-        <label for="ts">Timestamp track</label>
-      </form>
-    </div>
-    <table id="videos"></table>
-  </body>
-</html>
-

commit 1fe5ef8e947e6621b4c3c70bf2fae3d96ebf3ded

fix #79: errors when "infinite" selected on load
I've never seen this happen in Chrome; each load/reload starts fresh,
so infinite is never selected. But on Firefox it seems to remember the
setting across reloads, triggering this bug.

This bug was introduced in 58152e8: it started parsing/normalizing the
HTML form into a Javascript field on change. It didn't handle the
initial load properly. Prior to that commit, fetch() simply read
directly from the HTML form, so it didn't care about initial vs update.

ui-src/lib/views/NVRSettingsView.js
@@ -84,11 +84,11 @@ export default class NVRSettingsView {
    */
   wireControls_() {
     const videoLengthEl = $(`#${this.ids_.videoLenId}`);
-    this.videoLength_ = this.findSelectedOrFirst_(videoLengthEl);
+
+    function normalize(v) { return v == 'infinite' ? Infinity : Number(v); }
+    this.videoLength_ = normalize(this.findSelectedOrFirst_(videoLengthEl));
     videoLengthEl.change((e) => {
-      const newValueStr = e.currentTarget.value;
-      this.videoLength_ =
-        newValueStr == 'infinite' ? Infinity : Number(newValueStr);
+      this.videoLength_ = normalize(e.currentTarget.value);
       if (this.videoLengthHandler_) {
         this.videoLengthHandler_(this.videoLength_);
       }