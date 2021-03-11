984989dc1c06a6d142a97feaa598a3af879721a8
Scott Lamb <slamb@slamb.org>
2021-03-10 16:18:47 -0800

    small readability improvements to web.rs
    I think this has a minor behavior change: permission denied replies
    change to HTTP 403 where they were HTTP 401. The new behavior seems
    more correct, as these errors can occur when authentication has
    succeeded but the session in question is not authorized for the given
    operation. The UI currently doesn't care about this distinction.

--- server/base/error.rs
+++ server/base/error.rs
@@ -158,10 +158,10 @@ where
 #[macro_export]
 macro_rules! bail_t {
     ($t:ident, $e:expr) => {
-        return Err(failure::err_msg($e).context($crate::ErrorKind::$t).into());
+        return Err($crate::Error::from(failure::err_msg($e).context($crate::ErrorKind::$t)).into());
     };
     ($t:ident, $fmt:expr, $($arg:tt)+) => {
-        return Err(failure::err_msg(format!($fmt, $($arg)+)).context($crate::ErrorKind::$t).into());
+        return Err($crate::Error::from(failure::err_msg(format!($fmt, $($arg)+)).context($crate::ErrorKind::$t)).into());
     };
 }
 
