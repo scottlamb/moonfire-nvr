8d7e64ffd4c28fc05646dec69b3e5fa5e5dec8cd
michioxd <michio.haiyaku@gmail.com>
2024-04-13 22:45:57 +0700

    Resolved know problem.

--- ui/src/AppMenu.tsx
+++ ui/src/AppMenu.tsx
@@ -14,7 +14,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 import { LoginState } from "./App";
 import Box from "@mui/material/Box";
-import { useThemeMode } from "./components/ThemeMode";
+import { CurrentMode, useThemeMode } from "./components/ThemeMode";
 import { Brightness2, Brightness7, BrightnessAuto } from "@mui/icons-material";
 import { Tooltip } from "@mui/material";
 
@@ -29,7 +29,7 @@ interface Props {
 
 // https://material-ui.com/components/app-bar/
 function MoonfireMenu(props: Props) {
-  const { getTheme, changeTheme } = useThemeMode();
+  const { choosenTheme, changeTheme } = useThemeMode();
   const theme = useTheme();
   const [accountMenuAnchor, setAccountMenuAnchor] =
     React.useState<null | HTMLElement>(null);
@@ -79,7 +79,7 @@ function MoonfireMenu(props: Props) {
             color="inherit"
             size="small"
           >
-            {getTheme === 1 ? <Brightness7 /> : getTheme === 2 ? <Brightness2 /> : <BrightnessAuto />}
+            {choosenTheme === CurrentMode.Light ? <Brightness7 /> : choosenTheme === CurrentMode.Dark ? <Brightness2 /> : <BrightnessAuto />}
           </IconButton>
         </Tooltip>
         {props.loginState !== "unknown" && props.loginState !== "logged-in" && (
