60c6247ef9a11f14ec94981ede252ce74b13f3d7
michioxd <michio.haiyaku@gmail.com>
2024-04-13 23:42:13 +0700

    Fixed `TypeError: window.matchMedia is not a function` during testing via vitest.

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -7,3 +7,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { vi } from 'vitest'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
